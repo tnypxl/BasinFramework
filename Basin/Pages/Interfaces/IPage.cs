9f924ebe4098f3a1099e7619bfb98eadf45743ec
Arik Jones <tnypxl00@icloud.com>
2020-04-01 17:31:54 -0500

    Clean up page interfaces. Add #On interface method that allows switching to other pages in a fluent use case.

---
+++ Basin/Pages/Interfaces/IPage.cs
@@ -0,0 +1,17 @@
+namespace Basin.Pages.Interfaces
+{
+    /// <summary>
+    /// Interface for defining page object classes with element maps.
+    /// </summary>
+    public interface IPage
+    {
+    }
+    
+    public interface IPage<in TPageMap>
+    {
+        /// <summary>
+        /// Property to store page element map objects
+        /// </summary>
+        TPageMap Map { set; }
+    }
+}
