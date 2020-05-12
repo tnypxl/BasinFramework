8c74881060bb70e7d3f90531579a6c6b7c6c0707
Arik Jones <tnypxl00@icloud.com>
2020-05-12 16:36:41 -0500

    Refactor builders to support configuring remote drivers (e.g, for connecting to gridhub services) and proxies

--- Basin/Selenium/Interfaces/IDriverBuilder.cs
+++ Basin/Selenium/Interfaces/IDriverBuilder.cs
@@ -1,14 +1,108 @@
-using System.Runtime.InteropServices;
+using System;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
 
 namespace Basin.Selenium.Interfaces
 {
+    /// <summary>
+    ///     Builder interface for configuring
+    /// </summary>
     public interface IDriverBuilder
     {
-        void CreateService([Optional] object customDriverService);
+        Proxy DriverProxy { get; }
 
-        void CreateOptions([Optional] object customDriverOptions);
-        
-        IWebDriver GetDriver();
+        /// <summary>
+        ///     Method to returns a newly created <seealso cref="IWebDriver" /> instance
+        /// </summary>
+        IWebDriver GetDriver { get; }
+
+        /// <summary>
+        ///     Method to create driver service.
+        ///     <example>
+        ///         <code>
+        /// private FirefoxDriverService _driverService;
+        /// 
+        /// public void CreateService()
+        /// {
+        ///     _driverService = FirefoxDriverService.CreateDefaultService("path/to/driver/binary")
+        /// }
+        /// </code>
+        ///     </example>
+        /// </summary>
+        void CreateService();
+
+        /// <summary>
+        ///     Method to create driver options
+        ///     <example>
+        ///         <code>
+        /// private FirefoxOptions _driverOptions;
+        /// 
+        /// public void CreateOptions()
+        /// {
+        ///     _driverOptions = new FirefoxOptions();
+        /// }
+        /// </code>
+        ///     </example>
+        /// </summary>
+        void CreateOptions();
+
+        /// <summary>
+        ///     Method to configure driver to use a proxy
+        /// </summary>
+        void CreateProxy(Proxy proxy);
+
+        /// <summary>
+        ///     Method to return a newly created <seealso cref="RemoteWebDriver" /> instance
+        /// </summary>
+        /// <param name="uri"></param>
+        IWebDriver GetRemoteDriver(Uri uri);
+    }
+
+    public interface IChromeBuilder : IDriverBuilder
+    {
+        /// <summary>
+        ///     Gets ChromeDriverService object.
+        ///     Requires calling <seealso cref="IDriverBuilder.CreateOptions()" /> first.
+        /// </summary>
+        ChromeDriverService DriverService { get; }
+
+        /// <summary>
+        ///     Gets <see cref="ChromeOptions" />.
+        ///     Requires calling <seealso cref="IDriverBuilder.CreateService()" /> first.
+        /// </summary>
+        ChromeOptions DriverOptions { get; }
+    }
+
+    public interface IFirefoxBuilder : IDriverBuilder
+    {
+        /// <summary>
+        ///     Gets <see cref="FirefoxDriverService" /> object.
+        ///     Requires calling <seealso cref="IDriverBuilder.CreateService()" /> first.
+        /// </summary>
+        FirefoxDriverService DriverService { get; }
+
+        /// <summary>
+        ///     Gets <see cref="FirefoxOptions" />.
+        ///     Requires calling <seealso cref="IDriverBuilder.CreateOptions()" /> first.
+        /// </summary>
+        FirefoxOptions DriverOptions { get; }
+    }
+
+    public interface IInternetExplorerBuilder : IDriverBuilder
+    {
+        /// <summary>
+        ///     Gets <see cref="InternetExplorerDriverService" /> object.
+        ///     Requires calling <seealso cref="IDriverBuilder.CreateService()" /> first.
+        /// </summary>
+        InternetExplorerDriverService DriverService { get; }
+
+        /// <summary>
+        ///     Gets <see cref="InternetExplorerOptions" />.
+        ///     Requires calling <seealso cref="IDriverBuilder.CreateOptions()" /> first.
+        /// </summary>
+        InternetExplorerOptions DriverOptions { get; }
     }
 }
