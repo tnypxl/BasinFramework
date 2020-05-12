68ae9d83a63eca042fcb6e1fa50d09168dfe79c3
Arik Jones <tnypxl00@icloud.com>
2020-05-12 16:37:28 -0500

    Refactor builders to support configuring remote drivers (e.g, for connecting to gridhub services) and proxies

--- Basin/Selenium/Builders/InternetExplorerBuilder.cs
+++ Basin/Selenium/Builders/InternetExplorerBuilder.cs
@@ -1,27 +1,62 @@
+using System;
 using Basin.Selenium.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
 
 namespace Basin.Selenium.Builders
 {
-    public class InternetExplorerBuilder : IDriverBuilder
+    /// <summary>
+    ///     Concrete Driver Builder for InternetExplorer
+    /// </summary>
+    public class InternetExplorerBuilder : IInternetExplorerBuilder
     {
-        private InternetExplorerDriverService _service;
-        private InternetExplorerOptions _options;
-        
-        public void CreateService(object customDriverService = null)
+        private InternetExplorerOptions _driverOptions;
+        private Proxy _driverProxy;
+        private InternetExplorerDriverService _driverService;
+
+        /// <inheritdoc />
+        public void CreateService()
         {
-            _service = (InternetExplorerDriverService) customDriverService ?? InternetExplorerDriverService.CreateDefaultService(BSN.DriverPath);
+            _driverService = InternetExplorerDriverService.CreateDefaultService(BSN.DriverPath);
         }
 
-        public void CreateOptions(object customDriverOptions = null)
+        /// <inheritdoc />
+        public void CreateOptions()
         {
-            _options = (InternetExplorerOptions) customDriverOptions ?? new InternetExplorerOptions();
+            _driverOptions = new InternetExplorerOptions();
         }
 
-        public IWebDriver GetDriver()
+        /// <inheritdoc />
+        public void CreateProxy(Proxy proxy)
         {
-            return new InternetExplorerDriver(_service, _options);
+            _driverProxy = proxy;
+        }
+
+
+        /// <inheritdoc />
+        public IWebDriver GetDriver => new InternetExplorerDriver(DriverService, DriverOptions, TimeSpan.FromSeconds(BSN.Config.Driver.Timeout));
+
+
+        /// <inheritdoc />
+        public IWebDriver GetRemoteDriver(Uri uri)
+        {
+            return new RemoteWebDriver(uri, DriverOptions.ToCapabilities());
+        }
+
+        /// <inheritdoc />
+        public Proxy DriverProxy => _driverProxy ?? throw new NullReferenceException("_driverProxy is null. Create CreateProxy().");
+
+        /// <inheritdoc />
+        public InternetExplorerDriverService DriverService => _driverService ?? throw new NullReferenceException("_driverService is null. Call CreateService().");
+
+        /// <inheritdoc />
+        public InternetExplorerOptions DriverOptions => _driverOptions ?? throw new NullReferenceException("_driverOptions is null. Call CreateOptions()");
+
+        /// <inheritdoc cref="CreateProxy(Proxy)" />
+        public void CreateProxy()
+        {
+            CreateProxy(new Proxy());
         }
     }
 }
