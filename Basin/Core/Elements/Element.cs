adbc84ae5ecb9907d01588b1583fb8aab4c36512
Arik Jones <tnypxl00@icloud.com>
2020-08-06 17:06:45 -0500

    Framing out the basic structure of decorators

---
+++ Basin/Core/Elements/Element.cs
@@ -0,0 +1,39 @@
+﻿namespace Basin.Core.Elements
+{
+    public abstract class Element
+    {
+        public abstract void Click();
+
+        public abstract void SendKeys();
+
+        public abstract bool Displayed { get; }
+
+        public abstract string Text { get; }
+
+        public abstract Element WithClass(string className);
+
+        public abstract Element WithClass(params string[] classNames);
+
+        public abstract Element WithId(string id);
+
+        public abstract Element WithAttr(string attributeName);
+
+        public abstract Element WithAttr(string attributeName, string attributeValue);
+
+        public abstract Element WithDescendant(Element descendantElement);
+
+        public abstract Element WithChild(Element childElement);
+
+        public abstract Element Parent();
+
+        public abstract Element Parent(Element targetElement);
+
+        public abstract Element Child();
+
+        public abstract Element Child(Element targetElement);
+
+        public abstract Element Following(Element siblingElement);
+
+        public abstract Element Preceding(Element precedingElement);
+    }
+}
---
+++ Basin/Core/Elements/ElementDecorator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Basin.Core.Elements
+{
+    public class ElementDecorator : Element
+    {
+        protected Element _element;
+
+        protected ElementDecorator(Element element)
+        {
+            _element = element;
+        }
+
+        public override bool Displayed => throw new NotImplementedException();
+
+        public override string Text => throw new NotImplementedException();
+
+        public override void SendKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Click()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override Element Child()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element Child(Element targetElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element Following(Element siblingElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element Parent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element Parent(Element targetElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element Preceding(Element precedingElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element WithAttr(string attributeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element WithAttr(string attributeName, string attributeValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element WithChild(Element childElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element WithClass(string className)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element WithClass(params string[] classNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element WithDescendant(Element descendantElement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Element WithId(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
